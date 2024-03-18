32cf6fdfd4ad7cdb5cc7e8b0846ce0b6cdf048a0
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2024-03-18 10:47:30 -0500

    created function to get upset percent from csv

--- gameSims.py
+++ gameSims.py
@@ -1,4 +1,23 @@
+import pandas as pd
 import random
+
+def getUpsetPercent(seed1, seed2):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv("upset-percents.csv", encoding='utf-16')
+    favoriteSeed = min(seed1, seed2)
+    upsetSeed = max(seed1, seed2)
+    # Filter rows where the match_column matches the specified match_value
+    matching_rows = df.loc[(df['favorite_seed'] == favoriteSeed) & (df['underdog_seed'] == upsetSeed), 'upset_percent']
+    # Return values from the return_column for matching rows
+    if not matching_rows.empty:
+      return matching_rows.iloc[0]
+    else:
+      return '27%'  # or any default value you prefer
+
+print(getUpsetPercent(3,2))
+print(getUpsetPercent(1,6))
+print(getUpsetPercent(10,7))
+print(getUpsetPercent(6,4))
 
 def playGame(seed1, seed2):
   favoriteSeed = min(seed1, seed2)
@@ -9,3 +28,5 @@ def playGame(seed1, seed2):
     return underdogSeed
   else:
     return favoriteSeed
+  
+playGame(1,2)
