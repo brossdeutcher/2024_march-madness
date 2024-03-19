98a80524e97b64e7fb1da02f30ee4ba6bbe8100c
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2024-03-19 12:35:02 -0500

    connected upset percent lookup function to game sim function

--- gameSims.py
+++ gameSims.py
@@ -2,31 +2,26 @@ import pandas as pd
 import random
 
 def getUpsetPercent(seed1, seed2):
-    # Read the CSV file into a pandas DataFrame
     df = pd.read_csv("upset-percents.csv", encoding='utf-16')
+
     favoriteSeed = min(seed1, seed2)
     upsetSeed = max(seed1, seed2)
-    # Filter rows where the match_column matches the specified match_value
     matching_rows = df.loc[(df['favorite_seed'] == favoriteSeed) & (df['underdog_seed'] == upsetSeed), 'upset_percent']
-    # Return values from the return_column for matching rows
-    if not matching_rows.empty:
-      return matching_rows.iloc[0]
-    else:
-      return '27%'  # or any default value you prefer
 
-print(getUpsetPercent(3,2))
-print(getUpsetPercent(1,6))
-print(getUpsetPercent(10,7))
-print(getUpsetPercent(6,4))
+    if not matching_rows.empty:
+      if pd.isna(matching_rows.iloc[0]):
+        return 27
+      # print(f'rawVal: {matching_rows.iloc[0]} | seeds: {seed1}-{seed2}')
+      return int(matching_rows.iloc[0].rstrip('%'))
+    else:
+      return 27
 
 def playGame(seed1, seed2):
   favoriteSeed = min(seed1, seed2)
   underdogSeed = max(seed1, seed2)
   randNum = random.randint(1,100)
-  upsetPct = 27
+  upsetPct = getUpsetPercent(seed1, seed2)
   if randNum < upsetPct:
     return underdogSeed
   else:
     return favoriteSeed
-  
-playGame(1,2)
